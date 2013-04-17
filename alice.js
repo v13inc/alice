c4ac7a3243925b8474c627f27dfc99181af2a6c6
Sean Clark <clark.sean@gmail.com>
2013-04-17 10:57:47 -0700

    Refactor FTW
    I'll expand on most of the changes in the next commit message, but this
    turned out to be a very successful refactor. I've managed to clean up a
    LOT of the code, and got everything down to 400 lines! The parser is
    still a bit buggy, and everything needs to be properly namespaces, but
    its close to perfect (heh)!

--- alice.js
+++ alice.js
@@ -1,170 +1,36 @@
-// 
-// Alice syntax
+var exports = exports || {};
+DEBUG = exports.DEBUG = false; 
+var A = exports.Alice = {};
+
 //
-// Execution model:
-//  Code is broken up into lines and words. 
-//  Each line is executed in 2 phases:
+// Misc helpers
 //
-//  - parse phase
-//    The line is parsed from left -> right using a parser function. The parser
-//    function parses the line, and returns the next token. This is pushed onto
-//    the words list. Next, the parse function for the current word is called,
-//    which returns a parser function, which is used to parse the next token (etc..).
-//    Parsing stops once the end of the line is reached. 
-//  
-//  - execute phase
-//    After the words list is generated, words are popped off of the words list
-//    and executed from right -> left.
+each = function(list, callback) {
+  for(var i in list) callback(list[i], i);
+}
+repeat = function(num, callback) {
+  for(var i = 0; i < num; i++) callback(num);
+}
+
+//
+// Data structures
 //
 
-DEBUG = true;
-STYLE = true;
-
-// the entrance to the rabbit hole
-//
-// These functions are needed to bootstrap the list execution model
+// Stacks
 _values = __ = [];
-__push = function(item) {
-  __.push(item);
+_words = _w = [];
+
+// Dictionaries store definitions of words
+dicts = {
+  // run-time dicationaries
+  v: {}, // value
+  p: {}, // parse
+
+  // built-in value words
+  libv: {},
+  // built-in parse words
+  libp: {},
 }
-__drop = function() {
-  return __.pop();
-}
-arg = function() {
-  return __drop();
-}
-
-// implementation of list functions using the _values list for argument passing.
-// These helpers and the above functions effectively form an intermediate DSL that 
-// the rest of the virtual machine is built on.
-_ = function() {
-  __push([]);
-}
-
-_.out_of_bounds = "Out of bounds!";
-
-_.len = function() {
-  var list = arg(); __push(list.length);
-}
-
-_.push = function() {
-  var list = arg();
-  var item = arg();
-  list.push(item);
-}
-
-_.pop = function() {
-  var list = arg();
-  __push(list.pop());
-}
-
-_.drop = function() {
-  var list = arg();
-  return list.pop();
-}
-
-_.insert = function() {
-  var list = arg();
-  var index = arg();
-  var item = arg();
-
-  if(index < 0) index = list.length + index;
-  if(index == 0 || index > list.length + 1) return _.out_of_bounds;
-
-  list.splice(index - 1, 0, item);
-}
-
-_.set = function() {
-  var list = arg();
-  var index = arg();
-  var item = arg();
-
-  if(index < 0) index = list.length + index;
-  if(index == 0 || index > list.length + 1) return _.out_of_bounds;
-
-  list[index - 1] = item;
-}
-
-_.get = function() {
-  var list = arg();
-  var index = arg();
-
-  __push(list); _.len(); var len = arg();
-
-  if(index <= 0 || index > len) return _.out_of_bounds;
-
-  __push(list[index - 1]);
-}
-
-_.swap = function() {
-  var list = arg();
-  __push(list); _.pop(); var a = arg();
-  __push(list); _.pop(); var b = arg();
-  __push(a); __push(list); _.push();
-  if(typeof b != 'undefined') {
-    __push(b); __push(list); _.push();
-  }
-}
-
-
-// create a list to store parsed words
-_(); _words = arg();
-_(); _lines = arg();
-
-// shortcuts for dealing with the _values list
-__get = function() {
-  __push(__); _.get();
-}
-
-__set = function() {
-  __push(__); _.set();
-}
-
-__len = function() {
-  __push(__); _.len();
-}
-
-__swap = function() {
-  __push(__); _.swap();
-}
-
-// hashmap
-$ = function() {
-  __push({});
-}
-
-$.key_not_found = function() {
-  log('key not found!');
-}
-
-$.set = function() {
-  var hash = arg();
-  var key = arg();
-  var item = arg();
-
-  hash[key] = item;
-}
-
-$.get = function() {
-  var hash = arg();
-  var key = arg();
-
-  __push(hash[key] || $.key_not_found);
-}
-
-$(); $parse_vars = $p = arg();
-$(); $value_vars = $$ = arg();
-
-// convenience functions for var hashmaps
-$pget = function() { __push($p); $.get(); }
-$pset = function() { __push($p); $.set(); }
-
-$$get = function() { __push($$); $.get(); }
-$$set = function() { __push($$); $.set(); }
-
-// native implementations of words
-native_$$ = {}
-native_$p = {}
 
 //
 // Word helpers
@@ -178,50 +44,80 @@ native_$p = {}
 //  + 2 2 ` reordered prefix line after parse phase
 //
 
+//
+// Stack-based function helper
+//
+//  In order to ease the bootstrapping process, we rely on using the stack
+//  to pass arguments between functions, and avoid using the parent language
+//  features as much as possible. This can make code pretty ugly -- this helper
+//  cleans up the code by automatically popping the required number of values off
+//  the stack and passing them to the supplied function.
+//
+
+stackFunction = function(numArgs, callback) {
+  return function() {
+    var args = [];
+    repeat(numArgs, function() { args.push(__.pop()); });
+
+    callback.apply(this, args);
+  }
+}
+
+// helper to call built-in lib words. To avoid chicken-and-egg problems, it calls the manipulates the 
+// _values stack and calls the word block directly. Consequently, this helper only works with value words.
+$exec = function() {
+  var word = Array.prototype.shift.apply(arguments);
+  for(var i in arguments) {
+    __.push(arguments[i]);
+  }
+
+  dicts.libv[word]();
+}
+
+// run exec and pop a value of the _values stack
+$execPop = function() {
+  $exec.apply(this, arguments);
+  return __.pop();
+}
+
 $infix = function(word, block, extraParseBlock) {
   var valueWord = '_' + word;
   // create a parse block to flip arguments
   var parseBlock = function() {
-    __push(_words); _.drop(); // drop the word name from the _words list
-    __push(valueWord); __push(_words); _.push(); // push on the prefixed word to be called in the execute phase
-    __push(_words); _.swap(); // swap the first arg and the word to convert to prefix notation
+    $exec('_push', valueWord, _words); // push on the prefixed word to be called in the execute phase
+    $exec('_swap', _words); // swap the first arg and the word to convert to prefix notation
 
     if(extraParseBlock) extraParseBlock();
   }
 
-  native_$p[word] = parseBlock;
-  native_$$[valueWord] = block;
+  dicts.libv[valueWord] = block;
+  dicts.libp[word] = parseBlock;
 }
 
 $prefix = function(word, block, extraParseBlock) {
-  native_$$[word] = block;
-  if(extraParseBlock) native_$p[word] = extraParseBlock;
+  dicts.libv[word] = block;
+  if(extraParseBlock) dicts.libp[word] = extraParseBlock;
 }
 
 $postfix = function(word, block, extraParseBlock) {
   var valueWord = '_' + word;
-  var parseBlock = function() {
-    var line = arg();
-    var defaultParser = arg();
-
-    __push(_words); _.drop(); // drop the word name from the _words list
+  var parseBlock = stackFunction(1, function(line) {
     // insert the value word after the top 2 words on the _words list
-    __push(valueWord); __push(-3); __push(_words); _.insert();
+    $exec('_insert', valueWord, -3, _words);
 
-    __push(defaultParser);
-    __push(line);
+    __.push(line);
 
     if(extraParseBlock) extraParseBlock();
-  }
+  });
 
-  native_$p[word] = parseBlock;
-  native_$$[valueWord] = block;
+  dicts.libv[valueWord] = block;
+  dicts.libp[word] = parseBlock;
 }
 
 // parse-only word helper
 $parse = function(word, block, extraValueBlock) {
-  if(extraValueBlock) native_$$[word] = extraValueBlock;
-  native_$p[word] = block;
+  if(extraValueBlock) dicts.libv[word] = extraValueBlock;
+  dicts.libp[word] = block;
 }
 
 // helper to create parse functions that build blocks
@@ -239,205 +135,182 @@ $block = function(startWord, endWord, defer, extraParseBlock) {
     return i;
   }
 
-  $parse(startWord, function() {
-    var line = arg();
-
+  $parse(startWord, stackFunction(1, function(line) {
     // grab all the words until we hit the block end word
     var endBracketIndex = matchBracket(line, startWord, endWord, 1);
     var code = line.substr(0, endBracketIndex);
     line = line.substring(code.length + 2); // strip the block from the remaining line
-
-    // drop the bracket from the _words list
-    __push(_words); _.drop();
 
     // add the code block to the _words list
     var block = function() {
       execute(code);
     };
     if(defer) block.defer = true;
-    __push(block); __push(_words); _.push();
+    $exec('_push', block, _words);
 
-    __push(line);
+    __.push(line);
 
     if(extraParseBlock) extraParseBlock();
-  });
+  }));
 }
 
 // 
 // Native words
 //
-$infix('=', function() {
-  var word = arg();
-  var block = arg();
-  __push(block); __push(word); $$set();
-}, function() {
+
+// 
+// List Functions
+//
+$prefix('_', function() {
+  $exec('_push', [], __);
+});
+
+$infix('len', stackFunction(1, function(list) {
+  $exec('_push', list.length, __);
+}));
+
+$infix('push', stackFunction(2, function(list, item) {
+  list.push(item);
+}));
+
+$infix('pop', stackFunction(1, function(list) {
+  $exec('_push', list.pop(), __);
+}));
+
+$infix('drop', stackFunction(1, function(list) {
+  list.pop();
+}));
+
+$infix('insert', stackFunction(3, function(list, index, item) {
+  if(index < 0) index = list.length + index;
+  list.splice(index - 1, 0, item);
+}));
+
+$infix('set', stackFunction(3, function(list, index, item) {
+  if(index < 0) index = list.length + index;
+  list[index - 1] = item;
+}));
+
+// get
+$infix(':', stackFunction(2, function(list, index) {
+  $exec('_push', list[index - 1], __);
+}));
+
+$infix('swap', stackFunction(1, function(list) {
+  var a = $execPop('_pop', list);
+  var b = $execPop('_pop', list);
+  $exec('_push', a, list);
+  $exec('_push', b, list);
+}));
+
+// = adds a word to libv
+$infix('=', stackFunction(2, function(word, block) {
+  dicts.libv[word] = block;
+}), function() {
   // add a ' to the first argument, so we can overwrite existing words
-  __push(_words); _.pop(); var a = arg();
-  __push("'" + a); __push(_words); _.push();
+  var a = $execPop('_pop', _words);
+  $exec('_push', "'" + a, _words);
 });
 
-$infix('+', function() {
-  var a = Number(arg());
-  var b = Number(arg());
-  __push(a + b); 
-});
+// Math functions
+$infix('+', stackFunction(2, function(a, b) {
+  __.push(Number(a) + Number(b)); 
+}));
 
-$infix('-', function() {
-  var a = Number(arg());
-  var b = Number(arg());
-  __push(a - b); 
-});
+$infix('-', stackFunction(2, function(a, b) {
+  __.push(Number(a) - Number(b)); 
+}));
 
-$infix('*', function() {
-  var a = Number(arg());
-  var b = Number(arg());
-  __push(a * b); 
-});
+$infix('*', stackFunction(2, function(a, b) {
+  __.push(Number(a) * Number(b)); 
+}));
 
-$infix('/', function() {
-  var a = Number(arg());
-  var b = Number(arg());
-  __push(a / b); 
-});
+$infix('/', stackFunction(2, function(a, b) {
+  __.push(Number(a) / Number(b)); 
+}));
 
-$infix('%', function() {
-  var a = Number(arg());
-  var b = Number(arg());
-  __push(a % b); 
-});
+$infix('%', stackFunction(2, function(a, b) {
+  __.push(Number(a) % Number(b)); 
+}));
 
 $prefix('swap', function() {
-  __push(__); _.swap();
+  $exec('_swap', __);
 });
 
-$prefix('drop', function() {
-  __drop();
-});
+$prefix('drop', stackFunction(1, function(list) {
+  // no need to do anything here, since 'list' was popped off __
+}));
 
-$prefix('dup', function() {
-  var item = arg();
-  __push(item); __push(item);
-});
+$prefix('dup', stackFunction(1, function(item) {
+  __.push(item); __.push(item);
+}));
 
-$prefix('print', function() {
-  console.log(arg());
-});
+$prefix('print', stackFunction(1, function(item) {
+  console.log(item);
+}));
 
-$prefix('_', function() {
-  _();
-});
-
-$infix('push', function() {
-  var list = arg();
-  var item = arg();
-  __push(item); __push(list); _.push();
-});
-
-$infix('pop', function() {
-  var list = arg();
-  __push(list); _.pop();
-});
-
-$prefix('!', function() {
-  var word = arg();
+// ! executes the block on the top of the _values stack
+$prefix('!', stackFunction(1, function(word) {
   if(typeof word == 'function') {
-    block = word;
+    var block = word;
   } else {
     // lookup the word
-    __push(word); $$get(); var block = arg();
-    if(block === $.key_not_found) block = native_$$[word];
+    block = dicts.libv[word];
   }
 
   if(typeof block == 'function') block();
-});
+}));
 
-$prefix('!!', function() {
-  var block = arg();
-  __push(block); __push(block);
-  native_$$['!']();
-});
+// !! is like !, but keeps the block on the stack
+$prefix('!!', stackFunction(1, function(block) {
+  __.push(block); __.push(block);
+  dicts.libv['!']();
+}));
 
-$parse("'", function() {
-  var line = arg();
+// ' defers the next word from executing
+$parse("'", stackFunction(1, function(line) {
+  __.push("'" + line.trim());
+}));
 
-  // drop the ' from the _words list
-  __push(_words); _.drop();
-
-  __push("'" + line.trim());
-});
-
-$parse('"', function() {
-  var line = arg();
-
-  // drop the " from the _words list
-  __push(_words); _.drop();
+// " quotes strings and stops them from being parsed.
+$parse('"', stackFunction(1, function(line) {
+  // grab all the text until the ending quote
   var str = token(line, '"');
   var line = line.substring(str.length + 2).trim();
 
-  __push(str); __push(_words); _.push();
+  // push the quoted string onto _words
+  $exec('_push', str, _words);
 
-  __push(line);
-});
+  __.push(line);
+}));
 
-$block('(', ')');
-$block("'(", ')', true);
-$block('{', '}', true);
-$block('\n', '\n', false, function() {
-  var line = arg();
-  line = '\n' + line;
-  __push(line);
-});
+// setup our code block words
+$block('(', ')'); // immediate execution
+$block("'(", ')', true); // defer execution
+$block('{', '}', true); // defer execution
 
 // helper functions
-log = function(str) {
-  console.log(str);
-}
-
-log_lists = function(lists, colors, textColor) {
-  if(STYLE) {
-    var textColor = textColor || 'black';
-    var format = '';
-    var values = [];
-    for(var l in lists) {
-      var list = lists[l];
-      var color = colors[l];
-
-      for(var i in list) {
-        var word = list[i];
-        format += '%c%s%c ';
-        values.push('background-color: ' + color +'; color: ' + textColor);
-        values.push(word);
-        values.push('background-color: white; color: black');
-      }
-    }
-
-    console.log.apply(console, [format].concat(values));
-  } else {
-    var logStrs = []; 
-    for(var l in lists) {
-      var list = lists[l];
-      logStrs.push(list.join(', '));
-    }
-    console.log(textColor + ': ' + logStrs.join(' | '));
+log_lists = function(lists, prefix) {
+  var prefix = prefix ? prefix + ': ' : '';
+  var logStrs = []; 
+  for(var l in lists) {
+    var list = lists[l];
+    logStrs.push(list.join(', '));
   }
+  console.log(prefix + logStrs.join(' | '));
 }
 
-log_list = function(list, color) {
-  log_lists([list], [color]);
+log_list = function(list, prefix) {
+  log_lists([list], prefix);
 }
 
-p = print_words_and_values = function(textColor) {
-  var textColor = textColor || 'black';
-  log_lists([_words, __], ['palegreen', 'khaki'], textColor);
+p = print_words_and_values = function(prefix) {
+  log_lists([_words, __], prefix);
 }
 
-pw = print_words = function() {
-  log_list(_words, 'yellow');
-}
-
-pv = print_values = function() {
-  log_list(__, 'blue');
-}
+// 
+// Parser and Executor
+//
 
 token = function(line, delimiters) {
   // ensure delimeters is an array
@@ -462,73 +335,65 @@ token = function(line, delimiters) {
 }
 
 parser = function() {
-  var program = arg();
+  var program = __.pop();
 
   if(program) {
-    var word = token(program, [' ', '\t', '\n']);
+    var word = token(program, [' ', '\t', '\n']).trim();
     var delimeter = program[word.length];
     var lastWord = word.length == program.length;
     if(word != '') {
-      // push word onto the _words list
-      __push(word.trim()); __push(_words); _.push();
       // trim the word from the program
       program = program.substring(word.length + 1);
+      // push the remaining program back onto the stack
+      if(program) __.push(program);
 
       // call the parse function if it exists
-      var p_block = native_$p[word.trim()];
+      var p_block = dicts.libp[word];
       if(p_block) {
-        // supply the default parser and the current line
-        __push(program);
-
         p_block();
-
-        program = arg();
+      } else {
+        // push word onto the _words list
+        $exec('_push', word, _words);
       }
 
-      if(DEBUG) print_words_and_values('blue');
+      if(DEBUG) print_words_and_values('parse');
 
       if(delimeter == '\n' || lastWord) executer();
-      __push(program);
-      parser();
+      if(program) parser();
     }
   }
 }
 
 executer = function() {
-  __push(_words); var word = _.drop();
+  var word = $execPop('_pop', _words);
 
   // if the word is a block, execute it. Otherwise look it up in the variables
   if(typeof word == 'function' && !word.defer) {
     word();
   } else if(word) {
-    __push(word); $$get(); var block = arg();
+    var block = dicts.v[word] || dicts.libv[word];
 
-    if(block === $.key_not_found) block = native_$$[word];
-    
     if(block) {
       // if we found a block, execute it
-      typeof block == 'function' ? block() : __push(block);
+      typeof block == 'function' ? block() : __.push(block);
     } else {
       // push the word onto __ as a literal value, and remove the leading ', if needed
       if(word.indexOf && word.indexOf("'") == 0) word = word.substr(1);
-      __push(word);
+      __.push(word);
     }
 
-    if(DEBUG) print_words_and_values('red');
+    if(DEBUG) print_words_and_values('exec');
 
     executer();
   }
 }
 
 e = execute = function(program) {
-  __push(program.trim());
+  __.push(program.trim());
   parser();
   executer();
 }
 
-if(exports) {
-  exports.execute = execute;
-  exports._values = _values;
-  STYLE = false;
-  DEBUG = false;
-}
+exports.execute = execute;
+exports._values = _values;
+exports._words = _values;
